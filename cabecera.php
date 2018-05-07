20c68fea32579be04b9e2166c2fb9f223b976050
Ivan Herrera <Ivan Herrera@192.168.1.67>
2018-05-07 12:34:19 -0500

    se agregaron los bonres restantes y se agrego cabezera

---
+++ cabecera.php
@@ -0,0 +1,13 @@
+
+<a href="http://artedrez260.com">
+<img src="http://artedrez260.com/ratingOnline/artedrez260.jpg" alt="Artedrez 260" width="350px" height="100px" title="Ajedrez en Mexico"/></a>
+<a href="https://www.facebook.com/ajedrez.mexico">
+<img src="http://artedrez260.com/ratingOnline/share-chess-facebook_125.jpg" width="100px" height="100px" title="Facebook Ajedrez en Mexico"  alt="Facebook Ajedrez en Mexico" /></a>
+<a href="https://twitter.com/artedrez260">
+<img src="http://artedrez260.com/ratingOnline/twitter_chess_125.png" width="100px" height="100px" title="Twitter Ajedrez en Mexico" alt="Twitter Ajedrez en Mexico" /></a>
+<a href="http://artedrez260.com/ratingOnline/">
+<img src="http://artedrez260.com/ratingOnline/ratingFenamac.png" width="100px" height="100px" title="Rating Nacional en Linea" alt="Rating Nacional en Línea" /></a>
+<a href="http://artedrez260.com/eloCalculation">
+<img src="http://artedrez260.com/ratingOnline/eloCalculator_125.png" width="100px" height="100px" title="Calculador de Elo" alt="Calculador de Elo y Rating" /></a>
+<a href="http://artedrez260.com/topAjedrecistasEstado">
+<img src="http://artedrez260.com/ratingOnline/topEstado_125.png" width="100px" height="100px" title="Top ajedrecistas por Estado" alt="Lista del top de ajedrecistas por Estado" /></a>
--- index.php
+++ index.php
@@ -1,17 +1,60 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="witdh=device-witdh, user-scalable=no, initial-scalable=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <title></title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div id="cen">
+     <?php include('cabecera.php');?>
+         </div>      
+        <div class="container">
+            <div class=" row">
+                <div class=" col-xs-12 col-sm-8 col-md-8">
+                    <h1>
+                        Rating FENAMAC en línea
+                    </h1>
+                    <hr class="bg-info">
+                    <form action="buscarRating.php" method="post" class="" name="form1">
+                        <div>
+                        <p class="text-danger small">*Campo Obligatorio </p>
+                        <label for="nombrec">Nombre Completo:</label>
+                        <input class="form-control" id="inputap" name="input_nombrec" placeholder="apellido paterno, apellido materno, nombre" required="" ><br>
+                        </div>
+                         <div class="alert alert-primary" role="alert">
+                                Aplicar Filtro selecciona un campo 
+                         </div>
+                        <div>
+                        <input name="id" type="checkbox" checked="checked" >Id&nbsp;
+                        <input name="no" type="checkbox" checked="checked" >Nombre&nbsp;
+                        <input name="ti" type="checkbox" >T&iacute;tulo&nbsp;
+                        <input name="as" type="checkbox" checked="checked" >Asociaci&oacute;n&nbsp;
+                        <input name="ra" type="checkbox" checked="checked" >Rating&nbsp;
+                        <input name="ge" type="checkbox" >G&nbsp;
+                        <input name="fn" type="checkbox" >Fecha de Nacimiento&nbsp;
+                        <input name="fl" type="checkbox" >Flags
+                        </div>
+                        <button type="button" class="btn btn-warning" onClick="validar()">Consultar Rating</button>
+                    </form>
+                </div>
+                <div class="col-xs-12 col-sm-8 col-md-4">
+                    <p> 
+                        <b>Id:</b> Número de Identificación en la lista de la FENAMAC<br>
+                        <b>Nombre:</b> Nombre y Apellidos del Ajedrecista <br>
+                        <b>Título:</b> El título que ostenta como maestro, puede ser; g(Gran), m(Internacional), f(Fide), n(Nacional), c(Candidato) <br>
+                        <b>Asociación:</b> Asociación a la cual está inscrito el Ajedrecista. <br>
+                        <b>Rating:</b> Nacional o FIDE, mostrará el más alto. <br>
+                        <b>G:</b> Partidas jugadas que se tomaron en cuenta para el nuevo Rating. <br>
+                        <b>Fecha de Nacimiento:</b>> Fecha de Nacimiento del Ajedrecista. <br>
+                        <b>Flags:</b> Banderas.</p>
+                </div>
+            </div>
+        </div>
+
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </body>
-</html>
+</html>	
