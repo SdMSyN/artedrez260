fe54a004854a8124d872797f797d2d59c825187b
Ivan Herrera <Ivan Herrera@192.168.1.67>
2018-05-14 12:58:52 -0500

    ya se cuenta con la tabla para mostrar el resultado de la consulta tambien funciona los checkbox

--- index.php
+++ index.php
@@ -13,48 +13,72 @@
         <div class="container">
             <?php include('cabecera.php'); ?>   
             <div class=" row">
-                <div class=" col-xs-12 col-sm-8 col-md-8">
+                <div class=" col-xs-12 col-sm-12 col-md-12">
                     <h1>
                         Rating FENAMAC en línea
                     </h1>
                     <hr class="bg-info">
-                    <form action="consulta.php" method="post" class="" name="form1">
+                    <form action="Consulta.php" method="post" class="" name="form1">
                         <div>
                             <p class="text-danger small">*Campo Obligatorio </p>
                             <label for="nombrec">Nombre Completo:</label>
-                            <input class="form-control" id="nombre_completo" name="nombre" placeholder="Nombre Apellido Paterno Apellido Materno" required="" ><br>
+                            <input class="form-control" id="name" name="nombrec" placeholder="Nombre Apellido Paterno Apellido Materno" required="" ><br>
                         </div>
                         <div class="alert alert-primary" role="alert">
                             Aplicar Filtro selecciona un campo 
                         </div>
-                        <div>
-                            <input name="id" type="checkbox" checked="checked" >Id&nbsp;
-                            <input name="no" type="checkbox" checked="checked" >Nombre&nbsp;
-                            <input name="ti" type="checkbox" >T&iacute;tulo&nbsp;
-                            <input name="as" type="checkbox" checked="checked" >Asociaci&oacute;n&nbsp;
-                            <input name="ra" type="checkbox" checked="checked" >Rating&nbsp;
-                            <input name="ge" type="checkbox" >G&nbsp;
-                            <input name="fn" type="checkbox" >Fecha de Nacimiento&nbsp;
-                            <input name="fl" type="checkbox" >Flags
-                        </div>
-                        <button type="submit" class="btn btn-warning" >Consultar Rating</button>
-                    </form>
-                </div>
-                <div class="col-xs-12 col-sm-8 col-md-4">
-                    <p> 
-                        <b>Id:</b> Número de Identificación en la lista de la FENAMAC<br>
-                        <b>Nombre:</b> Nombre y Apellidos del Ajedrecista <br>
-                        <b>Título:</b> El título que ostenta como maestro, puede ser; g(Gran), m(Internacional), f(Fide), n(Nacional), c(Candidato) <br>
-                        <b>Asociación:</b> Asociación a la cual está inscrito el Ajedrecista. <br>
-                        <b>Rating:</b> Nacional o FIDE, mostrará el más alto. <br>
-                        <b>G:</b> Partidas jugadas que se tomaron en cuenta para el nuevo Rating. <br>
-                        <b>Fecha de Nacimiento:</b>> Fecha de Nacimiento del Ajedrecista. <br>
-                        <b>Flags:</b> Banderas.</p>
-                </div>
-            </div>
-        </div>
 
-        <script src="js/jquery-3.3.1.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-    </body>
-</html>	
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="inlineCheckbox1" name="id" value="option1">
+                            <label class="form-check-label" for="inlineCheckbox1">Id</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2" name="nombre" value="option2">
+                            <label class="form-check-label" for="inlineCheckbox2">Nombre</label>
+                        </div>
+                        
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="inlineCheckbox3" name="titulo" value="option3">
+                            <label class="form-check-label" for="inlineCheckbox2">Titulo</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="checkbox" id="inlineCheckbox4" name="asociacion" value="option4"  >
+                                <label class="form-check-label" for="inlineCheckbox3">Asociacion</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="inlineCheckbox5" name="rating" value="option5">
+                                <label class="form-check-label" for="inlineCheckbox2">Rating</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="inlineCheckbox6"  name="g" value="option6" disabled>
+                                    <label class="form-check-label" for="inlineCheckbox2">G</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="inlineCheckbox7"  name="fecha" value="option7" disabled>
+                                    <label class="form-check-label" for="inlineCheckbox2">Fecha de Nacimiento</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="inlineCheckbox8" name="flags" value="option8" disabled>
+                                    <label class="form-check-label" for="inlineCheckbox2">Flags</label>
+                        </div> <br>
+                                        <button type="submit" class="btn btn-warning" >Consultar Rating</button>
+                                        </form>
+                                    </div> <br>
+                                    <br>
+                                    <div class="col-xs-12 col-sm-12 col-md-12">
+                                        <p> 
+                                            <b>Id:</b> Número de Identificación en la lista de la FENAMAC<br>
+                                            <b>Nombre:</b> Nombre y Apellidos del Ajedrecista <br>
+                                            <b>Título:</b> El título que ostenta como maestro, puede ser; g(Gran), m(Internacional), f(Fide), n(Nacional), c(Candidato) <br>
+                                            <b>Asociación:</b> Asociación a la cual está inscrito el Ajedrecista. <br>
+                                            <b>Rating:</b> Nacional o FIDE, mostrará el más alto. <br>
+                                            <b>G:</b> Partidas jugadas que se tomaron en cuenta para el nuevo Rating. <br>
+                                            <b>Fecha de Nacimiento:</b>> Fecha de Nacimiento del Ajedrecista. <br>
+                                            <b>Flags:</b> Banderas.</p>
+                                    </div>
+                                </div>
+
+                                <script src="js/jquery-3.3.1.min.js"></script>
+                                <script src="js/bootstrap.min.js"></script>
+                                </body>
+                                </html>	
